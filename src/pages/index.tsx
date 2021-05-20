45f30dcd298e04b78e7c51bf1703a291ed6bc50e
Natanael Silva <natanaelsilva.progjs@gmail.com>
2021-05-19 22:53:41 -0300

    Configurando o chakra-ui e criando tela da dash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,27 @@
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/input";
 
-export default function Home() {
+export default function SingIn() {
   return (
-   <h1>Dash go</h1>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input type="email" name="email" label="Email" />
+          <Input type="password" name="password" label="Senha" />
+        </Stack>
+
+        <Button type="submit" mt={6} colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
